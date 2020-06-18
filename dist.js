a04b1cf64c283a60e0a702754c48a873cdec76ba
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-06-18 22:39:37 +0200

    Remove unnecessary module.exports from dist file and make it executable on linux

--- dist.js
+++ dist.js
@@ -9,12 +9,16 @@ const path = require('path');
         await fs.mkdir(path.join(__dirname, "dist"), { recursive: true })
         const fileContentLib = await fs.readFile(path.join(__dirname, "updateProgressLib.js"))
         const fileContentMain = await fs.readFile(path.join(__dirname, "updateProgress.js"))
+        const updatedLibContent = fileContentLib
+        .toString()
+        .substring(0, fileContentLib.toString().indexOf("module.exports") - 1)
         const updatedMainContent = fileContentMain
             .toString()
             .substring(fileContentMain.toString().indexOf("// Main") - 1)
             .replace(/updateProgress\./g, '')
-        await fs.writeFile(path.join(__dirname, "dist", "updateProgress.js"),
-            fileContentLib.toString() +  updatedMainContent)
+        const outputPath = path.join(__dirname, "dist", "updateProgress.js")
+        await fs.writeFile(outputPath, updatedLibContent +  updatedMainContent)
+        await fs.chmod(outputPath, 0o755);
     } catch (error) {
         console.error(error)
         return process.exit(1)
