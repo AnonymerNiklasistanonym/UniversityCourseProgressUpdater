e01f2fbee470bf36ebb607a22e0af14c5ba9d2a4
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-06-19 21:14:16 +0200

    Copy json schema and example to dist directory when running dist

--- dist.js
+++ dist.js
@@ -6,7 +6,8 @@ const path = require('path');
 // Main method (async wrapper)
 (async () => {
     try {
-        await fs.mkdir(path.join(__dirname, 'dist'), { recursive: true })
+        const distDir = path.join(__dirname, 'dist')
+        await fs.mkdir(distDir, { recursive: true })
         const fileContentLib = await fs.readFile(path.join(__dirname, 'updateProgressLib.js'))
         const fileContentMain = await fs.readFile(path.join(__dirname, 'updateProgress.js'))
         const updatedLibContent = fileContentLib
@@ -16,9 +17,15 @@ const path = require('path');
             .toString()
             .substring(fileContentMain.toString().indexOf('// Main') - 1)
             .replace(/updateProgress\./g, '')
-        const outputPath = path.join(__dirname, 'dist', 'updateProgress.js')
+        const outputPath = path.join(distDir, 'updateProgress.js')
         await fs.writeFile(outputPath, updatedLibContent + updatedMainContent)
         await fs.chmod(outputPath, 0o755)
+        const jsonSchema = path.join(__dirname, 'progress.schema.json')
+        const jsonExample = path.join(__dirname, 'progress.json')
+        const jsonSchemaDist = path.join(distDir, 'progress.schema.json')
+        const jsonExampleDist = path.join(distDir, 'progress.json')
+        await fs.copyFile(jsonSchema, jsonSchemaDist)
+        await fs.copyFile(jsonExample, jsonExampleDist)
     } catch (error) {
         console.error(error)
         return process.exit(1)
